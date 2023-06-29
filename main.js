810806a7fd8b54ea950d1c1ac576d98deeb5226e
rohit-itobuz <rohit.bharti@itobuz.com>
2023-06-29 18:39:40 +0530

    update

--- main.js
+++ main.js
@@ -16,41 +16,40 @@ function addBorrowing(event) {
   // Get values from input fields
   const nameInput = document.getElementById('name');
   const amountInput = document.getElementById('amount');
-  const name = nameInput.value;
-  const amount = parseFloat(amountInput.value);
+  const name = nameInput.value.trim();
+  const amount = amountInput.value.trim();
 
   // Clear input fields when you click on add button
   nameInput.value = '';
   amountInput.value = '';
-  
-  let person = {
-    Name : name,
-    Amount : amount
-  
-};
 
   // Add record to the array
-    //  borrowRecords.push(person.Name, person.Amount);
+  if (name === "" || amount === "") {
+    alert("please enter in logical way");
+  }
+  else {
     borrowRecords.push({ name, amount });
 
-  // Update borrowing list display status or show function 
-  updateBorrowingList();
+    // Update borrowing list display status or show function 
+    updateBorrowingList();
+  }
 }
+
 
 // Function to update borrowing list display
 function updateBorrowingList() {
-  borrowList.innerHTML = '' 
+  borrowList.innerHTML = ''
 
   borrowRecords.forEach((record, index) => {
     // create container for storing borrowed records 
     const listItem = document.createElement('li');
     const nameSpan = document.createElement('span');
     const amountSpan = document.createElement('span');
-    let totalAmountSpan = document.createElement('span');
     const updateButton = document.createElement('button');
 
-    nameSpan.innerText = record.name + " " + "=>"+ " ";
-    amountSpan.innerText = ' ' +'₹' + record.amount;
+    temp = record.amount;
+    nameSpan.innerText = "Name:-" + " " + record.name + " " + "🔗🔗" + " ";
+    amountSpan.innerText = "Amount:-" + ' ' + '₹' + record.amount;
     updateButton.innerText = 'Update';
     updateButton.setAttribute('data-index', index);
     updateButton.addEventListener('click', updateBorrowing);
@@ -59,27 +58,28 @@ function updateBorrowingList() {
     listItem.appendChild(amountSpan);
     listItem.appendChild(updateButton);
     borrowList.appendChild(listItem);
-    borrowList.appendChild(totalAmountSpan);
+
   });
+
 }
 
 // Function to update a borrowing record
 function updateBorrowing(event) {
   const index = event.target.getAttribute('data-index');
-
   if (index !== null) {
     const newAmount = prompt('Enter the updated amount of that person:', borrowRecords[index].amount);
-    
-    if (!isNaN(newAmount)) {
-      borrowRecords[index].amount += " " + "+" + " " + "₹" + newAmount;
-    //   console.log(borrowRecords);
-    //   totalAmountSpan = (amount + newAmount);
-    //   console.log(totalAmountSpan);
 
+
+    if (!isNaN(newAmount)) {
+      borrowRecords[index].amount = Number(borrowRecords[index].amount) + Number(newAmount);
       updateBorrowingList();
     }
+
   }
 }
+
+
+
 
 
 
